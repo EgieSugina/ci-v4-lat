ea51f48feef7347accd558282be3c3f23ffcddea
Sylens <egiesugina704@gmail.com>
2023-10-19 16:44:23 +0700

    change base

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/ci-v4-lata/';
+    public string $baseURL = 'http://localhost/ci-v4-lat/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
