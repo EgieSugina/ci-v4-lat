840a65565792bd9da0d68172a541abdc9ae1bc61
bootleg <egiesugina704@gmail.com>
2023-10-26 15:43:36 +0700

    add

--- app/Models/MatakuliahModel.php
+++ app/Models/MatakuliahModel.php
@@ -8,10 +8,10 @@ class MatakuliahModel extends Model
 {
     protected $table = "tbl_matakuliah";
     protected $allowedFields = ['kode_mk', 'nama_mk', 'sks', 'ruangan'];
+    protected $primaryKey = 'id_mk';
 
     public function data_mk($id_mk)
     {
-
         return $this->find($id_mk);
     }
     public function update_data($data, $id_mk)
